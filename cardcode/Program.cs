897ce5ed3711e1459dbb8af626fb093f55109ffe
Webbney Vallon <webbvallon1999@gmail.com>
2025-09-09 23:56:35 -0400

    initial commit

---
+++ cardcode/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Security;
+using Cards2;
+
+Card c1 = new Card(Rank.Ace, Suit.Diamonds);
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(c1.Rank + " " + c1.Suit);
